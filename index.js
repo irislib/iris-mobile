169502c42ee81b22e3219f13d80ef5587c242f70
Martti Malmi <sirius@iki.fi>
2020-03-31 14:16:28 +0300

    add bugsnag

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
- import 'react-native-get-random-values'
+import 'react-native-get-random-values'
 import '@gooddollar/gun-asyncstorage'
+import { Client } from 'bugsnag-react-native'
+const bugsnag = new Client("bb9298087eb87d2c78929a201ddb3e88")
 import { AppRegistry } from 'react-native'
 import App from './App/App'
 import { name as appName } from './app.json'
