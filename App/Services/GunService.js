d24f8043c76c73240ef763292f31df08cec7445b
Martti Malmi <sirius@iki.fi>
2020-05-06 14:21:57 +0300

    add gun from nodejs-mobile

--- App/Services/GunService.js
+++ App/Services/GunService.js
@@ -2,6 +2,6 @@ import {Gun, SEA} from 'gun/browser.ios.js';
 import GunOpen from 'gun/lib/open';
 import GunLoad from 'gun/lib/load';
 
-const gun = new Gun(['https://gun-us.herokuapp.com/gun']);
+const gun = new Gun(['https://gun-us.herokuapp.com/gun', 'http://localhost:8765']);
 
 export default gun;
--- nodejs-assets/nodejs-project/main.js
+++ nodejs-assets/nodejs-project/main.js
@@ -2,6 +2,9 @@
 // The main.js file will be overwritten in updates/reinstalls.
 
 var rn_bridge = require('rn-bridge');
+var Gun = require("gun");
+var server = require('http').createServer(Gun.serve);
+var gun = Gun({web: server.listen(8765), multicast: { port: 8765 } }); // file: userDataPath + '/radata', 
 
 // Echo every message received from react-native.
 rn_bridge.channel.on('message', (msg) => {
